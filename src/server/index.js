339af595cc737e21a389311a1bc8c0f60d229425
Kayk <ksm.kayk@outlook.com>
2018-01-31 19:11:42 -0200

    teste

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const server = restify.createServer()
 const routes = require('../http/routes')
 const cors = require('./cors')
 
+
 server.pre(cors.preflight)
 server.use(cors.actual)
 server.use(restify.plugins.bodyParser())
