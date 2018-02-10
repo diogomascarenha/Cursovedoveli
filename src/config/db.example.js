d489555946f71f2c2fb33abaec59edca241a9ac2
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2018-02-10 17:50:30 -0200

    Adicionada uma configuração de docker-compose com o mysql e o phpmyadmin

--- src/config/db.example.js
+++ src/config/db.example.js
@@ -2,8 +2,8 @@ const configDb = {
     host: 'localhost',
     port: 3306,
     database: 'curso',
-    user: 'root',
-    password: 'root'
+    user: 'curso',
+    password: 'secret'
 }
 
 module.exports = configDb
